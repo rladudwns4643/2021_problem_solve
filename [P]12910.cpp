847103404396c868c1ebe3e3b253dc236bb345c1
vscode KYJ <rladudwns4643@gmail.com>
2022-01-19 19:37:12 +0900

    Create [P]12910.cpp
    time: 5min
    etc: 단순구현

---
+++ [P]12910.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+
+    for (auto& a : arr) {
+        if (a % divisor == 0) answer.push_back(a);
+    }
+    if (answer.size() == 0) answer.push_back(-1);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
