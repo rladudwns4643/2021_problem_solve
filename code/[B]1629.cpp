b42f92d32eb6a965bdc7fbb97048394e57498fb9
Kim Young June <rladudwns4643@gmail.com>
2022-06-25 00:30:19 +0900

    Create [B]1629.cpp
    time: 20min
    etc: -

---
+++ code/[B]1629.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll POW(ll a, ll b, ll m) {
+	if (b == 1) return a % m;
+	ll val = POW(a, b / 2, m);
+	val = val * val % m;
+	if (b % 2 == 0) return val;
+	return val * a % m;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	ll a, b, m;
+	cin >> a >> b >> m;
+	cout << POW(a, b, m);
+}
