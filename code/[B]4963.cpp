5652e0470706cbc310211aa024854b75de01d4dc
Kim Young June <rladudwns4643@gmail.com>
2022-07-01 00:49:43 +0900

    Update [B]4963.cpp
    time: 10min
    etc: ans = 0 처리 때문에 제출하면 오류 발생

--- code/[B]4963.cpp
+++ code/[B]4963.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
 
-int w, h, ans;
+int w, h, ans = 0;
 int graph[51][51];
 bool visit[51][51];
 typedef pair<int, int> pii;
@@ -16,13 +16,13 @@ void bfs(int h, int w) {
 	q.push({ h,w });
 	visit[h][w] = true;
 	while (!q.empty()) {
-		h = q.front().first;
-		w = q.front().second;
+		int ch = q.front().first;
+		int cw = q.front().second;
 		q.pop();
 
 		for (int i = 0; i < 8; ++i) {
-			int nh = h + dir[i].first;
-			int nw = w + dir[i].second;
+			int nh = ch + dir[i].first;
+			int nw = cw + dir[i].second;
 
 			if (0 <= nh && 0 <= nw && nh < 50 && nw < 50) {
 				if (graph[nh][nw] && !visit[nh][nw]) {
@@ -62,16 +62,17 @@ int main() {
 		}
 		for (int i = 0; i < h; ++i) {
 			for (int j = 0; j < w; ++j) {
-				if (graph[i][j] == 1 && visit[i][j] == 0) {
-					ans++;
+				if (graph[i][j] == 1 && visit[i][j] == false) {
 					bfs(i, j);
+					ans++;
 				}
 			}
 		}
-		cout << ans;
+		cout << ans << "\n";
 
 		fill(&graph[0][0], &graph[50][51], 0);
 		fill(&visit[0][0], &visit[50][51], false);
 		ans = 0;
 	}
+	return 0;
 }
