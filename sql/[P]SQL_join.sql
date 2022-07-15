b353fd1d257171e8a12e03112d3534b6ee824a87
Kim Young June <rladudwns4643@gmail.com>
2022-07-16 02:06:16 +0900

    Update [P]SQL_join.sql
    time: 60min
    etc: 면접 대비 DB연습

--- sql/[P]SQL_join.sql
+++ sql/[P]SQL_join.sql
@@ -9,3 +9,14 @@ SELECT animal_outs.animal_id, animal_outs.name from animal_outs
 left outer join animal_ins on (animal_outs.animal_id = animal_ins.animal_id)
 where animal_outs.datetime < animal_ins.datetime
 order by animal_ins.datetime asc
+
+#3
+SELECT animal_ins.name, animal_ins.datetime
+from animal_ins left join animal_outs on animal_ins.animal_id = animal_outs.animal_id
+where animal_outs.animal_id is null
+order by animal_ins.datetime asc limit 3
+
+#4
+SELECT animal_ins.animal_id, animal_ins.animal_type, animal_ins.name
+from animal_ins join animal_outs on animal_ins.animal_id = animal_outs.animal_id
+where sex_upon_intake in('Intact Male', 'Intact Female') and sex_upon_outcome in ('Spayed Female', 'Neutered Male')
